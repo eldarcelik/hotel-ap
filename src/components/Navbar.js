f55cfbfd3ff1caa7a7f3941209444957f13edfb4
Eldar Celik <eldar.celik21@gmail.com>
2020-10-05 17:59:43 +0200

    Routing and Navbar finished

---
+++ src/components/Navbar.js
@@ -0,0 +1,36 @@
+import React, { Component, useState } from 'react';
+import { Link } from 'react-router-dom';
+import { FaAlignRight } from 'react-icons/fa';
+import { SiHotelsDotCom } from 'react-icons/si';
+
+export default function Navbar() {
+    const [isOpen, setIsOpen] = useState(false);
+    
+    const handleToogle = () => {
+        setIsOpen(!isOpen);
+    }
+
+    return (
+        <nav className="navbar">
+            <div className="nav-center">
+                <div className="nav-header">
+                    <Link to="/" className="nav-logo">
+                        <SiHotelsDotCom />OTEL<span> AP</span>
+                    </Link>
+                    <button type="button" className="nav-btn" onClick={handleToogle}>
+                        <FaAlignRight className="nav-icon"/>
+                    </button>
+                </div>
+                <ul className={isOpen ? "nav-links show-nav" : "nav-links"}>
+                    <li>
+                        <Link to="/">Home</Link>
+                    </li>
+                    <li>
+                        <Link to="/rooms">Rooms</Link>
+                    </li>
+                    
+                </ul>
+            </div>
+        </nav>
+    )
+}
