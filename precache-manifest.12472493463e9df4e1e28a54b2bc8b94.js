2694b3e4e747896dfd5dbb984cfd2e4cd8c01ce1
Eldar Celik <eldar.celik21@gmail.com>
2020-10-20 11:59:33 +0200

    Updates

---
+++ precache-manifest.12472493463e9df4e1e28a54b2bc8b94.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a6ac49e041eaa86d7285d9d04b3b7fc9",
+    "url": "/hotel-ap/index.html"
+  },
+  {
+    "revision": "3282728c517e12d48d75",
+    "url": "/hotel-ap/static/css/2.fb346933.chunk.css"
+  },
+  {
+    "revision": "c6ac9a72802c8e9ae03b",
+    "url": "/hotel-ap/static/css/main.42ccbc61.chunk.css"
+  },
+  {
+    "revision": "3282728c517e12d48d75",
+    "url": "/hotel-ap/static/js/2.0806550a.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/hotel-ap/static/js/2.0806550a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c6ac9a72802c8e9ae03b",
+    "url": "/hotel-ap/static/js/main.55d719cb.chunk.js"
+  },
+  {
+    "revision": "afaadd08ddca42037c1d",
+    "url": "/hotel-ap/static/js/runtime-main.029e80f3.js"
+  },
+  {
+    "revision": "67ce8de72d48ce18a3270e0fdf27ea94",
+    "url": "/hotel-ap/static/media/defaultBcg.67ce8de7.jpg"
+  },
+  {
+    "revision": "e9ea73949de71d45ae46047646b95727",
+    "url": "/hotel-ap/static/media/loading-arrow.e9ea7394.gif"
+  },
+  {
+    "revision": "7b17c49f01aa17fc11df8400d604e1c9",
+    "url": "/hotel-ap/static/media/room-9.7b17c49f.jpeg"
+  }
+]);
